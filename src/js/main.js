d90047cfa52605ee67f96a3cdcb2e84de7c43ebd
RA404 <61162669+RA404@users.noreply.github.com>
2020-07-09 11:02:11 +0500

    markup completed (#1)
    * configured webpack and infrastructure
    
    * Main page completed
    
    * the news page comleted
    
    * added overlay for mobile menu
    
    * all popups are completed(PC version)
    
    * all mobile popups are completed
    
    * disabled status for popup button completed
    
    * builded production version
    
    * filled the readme file and fixed browsers versions in babel
    
    * filled readme file
    
    * fixed versions of dependencies
    
    * fixed markup validation errors
    
    * added dist to gitignore
    
    * the project can be built(production mode) using the two commands build and start
    
    * edited readme: add link to github pages
    
    Co-authored-by: RA404 <Repin404@gmail.com>

---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+import '../pages/mainPage/index.css';
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: {
+    main: './src/js/main.js',
+    newsPage: './src/js/newsPage.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./images/[name].[ext]',
+            options: {
+              esModule: false,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/public/index.html',
+      filename: './index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/public/news/news.html',
+      filename: './news.html',
+      chunks: ['newsPage'],
+    }),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV':JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  devServer: {
+    contentBase: './src/public/',
+    port: 3000,
+  }
+};
