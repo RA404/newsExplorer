f3bc0da74b62e67f1d6d24b8743f6134cce67c18
RA404 <Repin404@gmail.com>
2020-09-04 11:35:02 +0500

    change apiURL

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 const apiKey = "72247f4ab53b48cf97e40059a80e1d5d";
-const apiURL = "https://praktikum.tk/news/v2/everything";
+const apiURL = "https://nomoreparties.co/news/v2/everything";
 const apiLinkLogin = "https://apinews.ra404.ru/users/me";
 const apiLinkSignup = "https://apinews.ra404.ru/signup";
 const apiLinkSignin = "https://apinews.ra404.ru/signin";
